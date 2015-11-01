dec7478767107366c2955fd0dbe44bfb2e9a9c85
slfmessi <slfmessi@hotmail.com>
2015-11-01 16:57:37 +0800

    basic feature

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ThemePackTransformer
+
+A shell script to transfer windows themepack(both .themepack and deskthemepack) into gnome slideshow wallpapers.
+
+Usage: ThemePackTransformer.sh your_themepack
+
+To use this, you need cabextract installed.
+
+Some features will be added, such as specified time interval, many thmepack into one slideshow, etc. And there is 
+some ugly code, it would be nice if you can help me with that.
---
+++ ThemePackTransformer.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+#
+# A script to transfer Windows themepack file to Gnome slideshowWallpapers
+# 
+# Remember: you need to have cabextract installed
+# for Ubuntu: apt-get install cabextract
+
+readonly file_type="jpg|png"
+readonly properties_folder=$HOME/.local/share/gnome-background-properties
+readonly content_folder=$HOME/.local/share/backgrounds # store all theme content
+
+theme_name=
+theme_folder=
+slideshow_xml=
+
+set_file_path() {
+  theme_file=$1
+  theme_fullname=${theme_file##*/}
+  theme_name=${theme_fullname%.*}
+  theme_folder=$content_folder/$theme_name
+  slideshow_xml=$content_folder/$theme_name/$theme_name.xml
+  
+  mkdir -p $content_folder/$theme_name
+  cabextract -d /tmp/$theme_name/ $1 > /dev/null && mv /tmp/$theme_name/DesktopBackground/* $content_folder/$theme_name/
+}
+
+# generate slideshow xml
+# param : 
+#    $1 : date string
+generate_slideshow_xml() {
+  date_str=$1
+  year=${date_str:0:4}
+  month=${date_str:5:2}
+  day=${date_str:8:2}
+  
+  echo "<background>"
+  echo "  <starttime>"
+  echo "    <year>$year</year>"
+  echo "    <month>$month</month>"
+  echo "    <day>$day</day>"
+  echo "    <hour>00</hour>"
+  echo "    <minute>00</minute>"
+  echo "    <second>00</second>"
+  echo "  </starttime>"
+  echo "<!-- This animation starts at midnight of $Yer/$Mnth/$Day.  -->"
+
+  # find all image files
+  find -L $content_folder/$theme_name \
+       -regextype posix-extended \
+       -iregex ".*\.($file_type)" \
+       | sort > $theme_folder/image_list
+
+  # count files and calculate interval
+  # 10min to change a picture
+  # this means 595s + 5s
+  static_time=595.0
+  transition_time=5.0
+  static_head="  <static>\n    <duration>$static_time</duration>\n    <file>"
+  static_tail="</file>\n  </static>"
+  from_head="  <transition>\n    <duration>$transition_time</duration>\n    <from>"
+  from_tail="</from>"
+  to_head="    <to>"
+  to_tail="</to>\n  </transition>"
+
+  image_count=$( cat $theme_folder/image_list | wc -l)
+  first_image=$( cat $theme_folder/image_list | head -n 1)
+  for (( i = 0; i < $image_count; i++ )); do
+    #statements
+    static_image=$( cat $theme_folder/image_list | sed -n `expr $i + 1`p )
+    to_image=$( cat $theme_folder/image_list | sed -n `expr $i + 2`p )
+    if [[ $i -eq `expr $image_count - 1` ]]; then
+      to_image=$first_image
+    fi
+    echo "  <static>"
+    echo "    <duration>$static_time</duration>"
+    echo "    <file>$static_image</file>"
+    echo "  </static>"
+    echo "  <transition>"
+    echo "    <duration>$transition_time</duration>"
+    echo "    <from>$static_image</from>"
+    echo "    <to>$to_image</to>"
+    echo "  </transition>"
+  done
+  echo "</background>"
+  rm $theme_folder/image_list
+}
+
+generate_properties_xml() {
+  # TODO: write the properties xml
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+  echo "<!DOCTYPE wallpapers SYSTEM \"gnome-wp-list.dtd\">"
+  echo "<wallpapers>"
+  echo "  <wallpaper>"
+  echo "    <name>$theme_name</name>"
+  echo "    <filename>$slideshow_xml</filename>"
+  echo "    <options>zoom</options>"
+  echo "    <shade_type>solid</shade_type>"
+  echo "  </wallpaper>"
+  echo "</wallpapers>"
+}
+
+# main
+mkdir -p $properties_folder
+date=`date +%Y-%m-%d`
+set_file_path $1
+generate_slideshow_xml $date > $slideshow_xml
+generate_properties_xml > $properties_folder/$theme_name-wallpapers.xml
